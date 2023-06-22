ddaa9feada7b71fef354fc48bb356144c1cb7e78
flyingfrog23 <andrinhilbrand@hotmail.com>
2023-06-22 23:43:05 +0200

    Update 00-database-backup.sh

--- 00-database-backup.sh
+++ 00-database-backup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # vmangos-docker
-# Copyright (C) 2021-present  Michael Serajnik  https://sr.ht/~mser/
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
