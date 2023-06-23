ad8ddd50aba057076a0bd0072f3efd1056c7c1da
flyingfrog23 <andrinhilbrand@hotmail.com>
2023-06-23 22:07:56 +0200

    Add files via upload

---
+++ docker/database/old/generate-db-2.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# vmangos-docker
+# Copyright (C) 2021-present  Michael Serajnik  https://sr.ht/~mser/
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#!/bin/bash
+echo "[VMaNGOS]: Importing databases..."
+
+echo "[VMaNGOS]: Importing logon..."
+mariadb -u mangos -p$MYSQL_ROOT_PASSWORD realmd < /opt/database/logon.sql
+
+echo "[VMaNGOS]: Importing logs..."
+mariadb -u mangos -p$MYSQL_ROOT_PASSWORD logs < /opt/database/logs.sql
+
+echo "[VMaNGOS]: Importing characters..."
+mariadb -u mangos -p$MYSQL_ROOT_PASSWORD characters < /opt/database/characters.sql
+
+echo "[VMaNGOS]: Importing world..."
+mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos < /opt/world_database/$VMANGOS_WORLD.sql
+
+echo "[VMaNGOS]: Importing database updates..."
+[ -e /opt/vmangos/sql/migrations/world_db_updates.sql ] && \
+  mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos < /opt/vmangos/sql/migrations/world_db_updates.sql
+[ -e /opt/vmangos/sql/migrations/characters_db_updates.sql ] && \
+  mariadb -u mangos -p$MYSQL_ROOT_PASSWORD characters < /opt/vmangos/sql/migrations/characters_db_updates.sql
+[ -e /opt/vmangos/sql/migrations/logon_db_updates.sql ] && \
+  mariadb -u mangos -p$MYSQL_ROOT_PASSWORD realmd < /opt/vmangos/sql/migrations/logon_db_updates.sql
+[ -e /opt/vmangos/sql/migrations/logs_db_updates.sql ] && \
+  mariadb -u mangos -p$MYSQL_ROOT_PASSWORD logs < /opt/vmangos/sql/migrations/logs_db_updates.sql
+
+echo "[VMaNGOS]: Upgrading mysql..."
+mariadb-upgrade -u mangos -p$MYSQL_ROOT_PASSWORD
+
+echo "[VMaNGOS]: Configuring default realm..."
+mariadb -u mangos -p$MYSQL_ROOT_PASSWORD -e \
+  "INSERT INTO realmd.realmlist (name, address, port, icon, realmflags, timezone, allowedSecurityLevel, population, gamebuild_min, gamebuild_max, flag, realmbuilds) VALUES ('$VMANGOS_REALM_NAME', '$VMANGOS_REALM_IP', '$VMANGOS_REALM_PORT', '$VMANGOS_REALM_ICON', '$VMANGOS_REALM_FLAGS', '$VMANGOS_TIMEZONE', '$VMANGOS_ALLOWED_SECURITY_LEVEL', '$VMANGOS_POPULATION', '$VMANGOS_GAMEBUILD_MIN', '$VMANGOS_GAMEBUILD_MAX', '$VMANGOS_FLAG', '');"
+
+echo "[VMaNGOS]: Database creation complete!"
