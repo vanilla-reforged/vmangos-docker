8b231958c922f8c3eabc44f259fab5ad20cb3816
flyingfrog23 <andrin.straessler@outlook.com>
2023-06-27 00:58:55 +0200

    Update 00-build-extract.sh

--- 00-build-extract.sh
+++ 00-build-extract.sh
@@ -18,7 +18,7 @@
 
 # get variables defined in .env_shell
 
-source .env_shell
+source .env
 
 # Handle script call from other directory
 
@@ -34,13 +34,6 @@ echo "[VMaNGOS]: Building VMaNGOS..."
 
 docker build \
   --build-arg DEBIAN_FRONTEND=noninteractive \
-  --no-cache \
-  -t vmangos_build \
-  -f ./docker/build/Dockerfile .
-
-# Run compiler image
-
-docker run \
   -v "$repository_path/volume/compiled_core:/compiled_core" \
   -v "$repository_path/volume/database:/database" \
   -v "$repository_path/volume/ccache:/ccache" \
@@ -57,8 +50,31 @@ docker run \
   --build-arg VMANGOS_SCRIPTS=$VMANGOS_SCRIPTS \
   --build-arg VMANGOS_LIBCURL=$VMANGOS_LIBCURL \
   --build-arg VMANGOS_WORLD_DATABASE=$VMANGOS_WORLD_DATABASE \
-  --rm \
-  vmangos_build
+  --no-cache \
+  -t vmangos_build \
+  -f ./docker/build/Dockerfile .
+
+# Run compiler image
+
+# docker run \
+#  -v "$repository_path/volume/compiled_core:/compiled_core" \
+#  -v "$repository_path/volume/database:/database" \
+#  -v "$repository_path/volume/ccache:/ccache" \
+#  --build-arg DEBIAN_FRONTEND=noninteractive \
+#  --build-arg VMANGOS_GIT_SOURCE_CORE_URL=$VMANGOS_GIT_SOURCE_CORE_URL \
+#  --build-arg VMANGOS_GIT_SOURCE_DATABASE_URL=$VMANGOS_GIT_SOURCE_DATABASE_URL \
+#  --build-arg CCACHE_DIR=$CCACHE_DIR \
+#  --build-arg VMANGOS_THREADS=$VMANGOS_THREADS \
+#  --build-arg VMANGOS_DEBUG=$VMANGOS_DEBUG \
+#  --build-arg VMANGOS_MALLOC=$VMANGOS_MALLOC \
+#  --build-arg VMANGOS_CLIENT=$VMANGOS_CLIENT \
+#  --build-arg VMANGOS_EXTRACTORS=$VMANGOS_EXTRACTORS \
+#  --build-arg VMANGOS_ANTICHEAT=$VMANGOS_ANTICHEAT \
+#  --build-arg VMANGOS_SCRIPTS=$VMANGOS_SCRIPTS \
+#  --build-arg VMANGOS_LIBCURL=$VMANGOS_LIBCURL \
+#  --build-arg VMANGOS_WORLD_DATABASE=$VMANGOS_WORLD_DATABASE \
+#  --rm \
+#  vmangos_build
 
   if [ $(ls -l ./volume/client_data_extracted | wc -l) -eq 1 ]; then
     echo "[VMaNGOS]: Extracted client data missing, running extractors."
