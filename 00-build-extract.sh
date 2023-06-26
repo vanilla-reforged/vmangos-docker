45afce17942a6f125135614feea24bfd43099612
flyingfrog23 <andrin.straessler@outlook.com>
2023-06-27 01:14:59 +0200

    Update 00-build-extract.sh

--- 00-build-extract.sh
+++ 00-build-extract.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# get variables defined in .env_shell
+# get variables defined in .env
 
 source .env
 
