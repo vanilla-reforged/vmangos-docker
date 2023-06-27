e162ffdc1f05d504e9d77508bb100804a2d978c4
flyingfrog23 <andrin.straessler@outlook.com>
2023-06-28 01:23:56 +0200

    Update 12-database_import_migrations-to-test.sh

--- 12-database_import_migrations-to-test.sh
+++ 12-database_import_migrations-to-test.sh
@@ -12,16 +12,17 @@ get_script_path() {
 repository_path=$(dirname "$(get_script_path "$0")")
 cd "$repository_path"
 
-docker exec -T vmangos_database bin/sh -c \
-  '[ -e /opt/core/sql/migrations/world_db_updates.sql ] mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos < /src/github_core/sql/migrations/world_db_updates.sql'
-docker exec -T vmangos_database sh -c \
-  '[ -e /opt/core/sql/migrations/characters_db_updates.sql ] mariadb -u mangos -p$MYSQL_ROOT_PASSWORD characters < /src/github_core/sql/migrations/characters_db_updates.sql'
-docker exec -T vmangos_database sh -c \ 'mariadb-dump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD realmd > /backup/"$date_time"_realmd.sql'
+date_time=$(date "+%Y.%m.%d-%H.%M.%S")
 
-docker exec -T vmangos_database sh -c
+echo "[VMaNGOS]: Importing migrations..."
+
+docker exec vmangos_database /bin/sh \
+  '[ -e /opt/core/sql/migrations/world_db_updates.sql ] mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos < /src/github_core/sql/migrations/"$date_time"_world_db_updates.sql'
+docker exec vmangos_database /bin/sh \
+  '[ -e /opt/core/sql/migrations/characters_db_updates.sql ] mariadb -u mangos -p$MYSQL_ROOT_PASSWORD characters < /src/github_core/sql/migrations/"$date_time"_characters_db_updates.sql'
+docker exec vmangos_database /bin/sh \ 
+'[ -e /opt/core/sql/migrations/logon_db_updates.sql ] mariadb -u mangos -p$MYSQL_ROOT_PASSWORD realmd < /src/github_core/sql/migrations/"$date_time"_logon_db_updates.sql'
+docker exec vmangos_database /bin/sh \ 
+'[ -e /opt/core/sql/migrations/logs_db_updates.sql ] mariadb -u mangos -p$MYSQL_ROOT_PASSWORD logs < /src/github_core/sql/migrations/"$date_time"_logs_db_updates.sql'
 
 echo "[VMaNGOS]: Importing database updates..."
-
-
-[ -e /opt/core/sql/migrations/logon_db_updates.sql ] mariadb -u mangos -p$MYSQL_ROOT_PASSWORD realmd < /src/github_core/sql/migrations/logon_db_updates.sql
-[ -e /opt/core/sql/migrations/logs_db_updates.sql ] mariadb -u mangos -p$MYSQL_ROOT_PASSWORD logs < /src/github_core/sql/migrations/logs_db_updates.sql
