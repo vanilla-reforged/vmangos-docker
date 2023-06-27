51495618f0dba580beb817cd2c483356fbbcf580
flyingfrog23 <andrin.straessler@outlook.com>
2023-06-28 01:15:09 +0200

    Update and rename 99-database-backup.sh_todo to 99-database-backup.sh

---
+++ 99-database-backup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#Get variables defined in .env
+
+source .env
+
+#Handle script call from other directory
+
+get_script_path() {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+repository_path=$(dirname "$(get_script_path "$0")")
+cd "$repository_path"
+
+date_time=$(date "+%Y.%m.%d-%H.%M.%S")
+
+echo "[VMaNGOS]: Backing up databases..."
+
+docker exec -T vmangos_database sh -c \
+  'mariadb-dump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD mangos > /backup/"$date_time"_mangos.sql'
+docker exec -T vmangos_database sh -c \
+  'mariadb-dump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD characters > /backup/"$date_time"_characters.sql'
+docker exec -T vmangos_database sh -c \
+  'mariadb-dump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD realmd > /backup/"$date_time"_realmd.sql'
+
+echo "[VMaNGOS]: Backup complete!"
