49e51bb847d0657221fdf690a88c54db18e667c0
flyingfrog23 <andrin.straessler@outlook.com>
2023-06-27 17:55:36 +0200

    Update 00-compile.sh

--- 00-compile.sh
+++ 00-compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# get variables defined in .env
+#Get variables defined in .env
 
 source .env
 
-# Handle script call from other directory
+#Handle script call from other directory
 
 get_script_path() {
   [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
@@ -14,7 +14,7 @@ cd "$repository_path"
 
 echo "[VMaNGOS]: Compiling VMaNGOS..."
 
-# Build compiler image
+#Build compiler image
 
 docker build \
   --build-arg DEBIAN_FRONTEND=noninteractive \
@@ -22,7 +22,7 @@ docker build \
   -t vmangos_build \
   -f ./docker/build/Dockerfile .
 
-# Run compiler image
+#Run compiler image
 
 docker run \
   -v "$repository_path/src/github_core:/src/github_core" \
