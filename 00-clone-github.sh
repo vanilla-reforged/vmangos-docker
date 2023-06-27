ca9d6ec9b7a2b796f9ef266470547ef6f834f2ad
flyingfrog23 <andrin.straessler@outlook.com>
2023-06-27 17:45:05 +0200

    Create 00-clone-github.sh

---
+++ 00-clone-github.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# get variables defined in .env
+
+source .env
+
+# Handle script call from other directory
+
+get_script_path() {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+repository_path=$(dirname "$(get_script_path "$0")")
+cd "$repository_path"
+
+echo "[VMaNGOS]: Cloning github repositories."
+
+git clone $VMANGOS_GIT_SOURCE_CORE_URL /src/github_core
+git clone $VMANGOS_GIT_SOURCE_DATABASE_URL /src/github_database
+
+echo "[VMaNGOS]: Cloning github repositories finished."
