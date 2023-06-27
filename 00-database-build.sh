f5f8f9f6e74b9abb7b7213c93ae96c8d2210bfe6
flyingfrog23 <andrin.straessler@outlook.com>
2023-06-27 18:13:46 +0200

    Create 00-database-build.sh

---
+++ 00-database-build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#Get variables defined in .env
+
+source .env
+
+#Handle script call from other directory
+
+get_script_path() {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+repository_path=$(dirname "$(get_script_path "$0")")
+cd "$repository_path"
+
+echo "[VMaNGOS]: Compiling VMaNGOS..."
+
+#Build compiler image
+
+docker build \
+  --build-arg DEBIAN_FRONTEND=noninteractive \
+  --no-cache \
+  -t vmangos_build \
+  -f ./docker/build/Dockerfile .
+
+#Run compiler image
+
+docker run \
+  -v "$repository_path/src/github_core:/src/github_core" \
+  -v "$repository_path/vol/cache:/vol/ccache" \
+  -v "$repository_path/vol/core:/vol/core" \
+  -e CCACHE_DIR=$CCACHE_DIR \
+  -e VMANGOS_ANTICHEAT=$VMANGOS_ANTICHEAT \
+  -e VMANGOS_CLIENT=$VMANGOS_CLIENT \
+  -e VMANGOS_DEBUG=$VMANGOS_DEBUG \
+  -e VMANGOS_EXTRACTORS=$VMANGOS_EXTRACTORS \
+  -e VMANGOS_LIBCURL=$VMANGOS_LIBCURL \
+  -e VMANGOS_MALLOC=$VMANGOS_MALLOC \
+  -e VMANGOS_SCRIPTS=$VMANGOS_SCRIPTS \
+  -e VMANGOS_THREADS=$VMANGOS_THREADS \
+  -e VMANGOS_WORLD_DATABASE=$VMANGOS_WORLD_DATABASE \
+  --rm \
+  vmangos_build
+
+echo "[VMaNGOS]: Compiling complete!"
