8c20958e0c442344bfbee5a66c219a6524fbd192
flyingfrog23 <andrin.straessler@outlook.com>
2023-06-27 13:59:34 +0200

    Update generate-db-2.sh

--- docker/database/generate-db-2.sh
+++ docker/database/generate-db-2.sh
@@ -19,32 +19,32 @@
 echo "[VMaNGOS]: Importing databases..."
 
 echo "[VMaNGOS]: Importing logon..."
-mariadb -u mangos -p $MYSQL_ROOT_PASSWORD realmd < /opt/core/sql/logon.sql
+mariadb -u mangos -p$MYSQL_ROOT_PASSWORD realmd < /opt/core/sql/logon.sql
 
 echo "[VMaNGOS]: Importing logs..."
-mariadb -u mangos -p $MYSQL_ROOT_PASSWORD logs < /opt/core/sql/logs.sql
+mariadb -u mangos -p$MYSQL_ROOT_PASSWORD logs < /opt/core/sql/logs.sql
 
 echo "[VMaNGOS]: Importing characters..."
-mariadb -u mangos -p $MYSQL_ROOT_PASSWORD characters < /opt/core/sql/characters.sql
+mariadb -u mangos -p$MYSQL_ROOT_PASSWORD characters < /opt/core/sql/characters.sql
 
 echo "[VMaNGOS]: Importing world..."
-mariadb -u mangos -p $MYSQL_ROOT_PASSWORD mangos < /opt/core/sql/$VMANGOS_WORLD.sql
+mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos < /opt/core/sql/$VMANGOS_WORLD.sql
 
 echo "[VMaNGOS]: Importing database updates..."
 [ -e /opt/core/sql/migrations/world_db_updates.sql ]
-  mariadb -u mangos -p $MYSQL_ROOT_PASSWORD mangos < /opt/core/sql/migrations/world_db_updates.sql
+  mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos < /opt/core/sql/migrations/world_db_updates.sql
 [ -e /opt/core/sql/migrations/characters_db_updates.sql ]
-  mariadb -u mangos -p $MYSQL_ROOT_PASSWORD characters < /opt/core/sql/migrations/characters_db_updates.sql
+  mariadb -u mangos -p$MYSQL_ROOT_PASSWORD characters < /opt/core/sql/migrations/characters_db_updates.sql
 [ -e /opt/core/sql/migrations/logon_db_updates.sql ]
-  mariadb -u mangos -p $MYSQL_ROOT_PASSWORD realmd < /opt/core/sql/migrations/logon_db_updates.sql
+  mariadb -u mangos -p$MYSQL_ROOT_PASSWORD realmd < /opt/core/sql/migrations/logon_db_updates.sql
 [ -e /opt/core/sql/migrations/logs_db_updates.sql ]
-  mariadb -u mangos -p $MYSQL_ROOT_PASSWORD logs < /opt/core/sql/migrations/logs_db_updates.sql
+  mariadb -u mangos -p$MYSQL_ROOT_PASSWORD logs < /opt/core/sql/migrations/logs_db_updates.sql
 
 echo "[VMaNGOS]: Upgrading mysql..."
-mariadb-upgrade -u mangos -p $MYSQL_ROOT_PASSWORD
+mariadb-upgrade -u mangos -p$MYSQL_ROOT_PASSWORD
 
 echo "[VMaNGOS]: Configuring default realm..."
-mariadb -u mangos -p $MYSQL_ROOT_PASSWORD -e \
+mariadb -u mangos -p$MYSQL_ROOT_PASSWORD -e \
   "INSERT INTO realmd.realmlist (name, address, port, icon, realmflags, timezone, allowedSecurityLevel, population, gamebuild_min, gamebuild_max, flag, realmbuilds) VALUES ('$VMANGOS_REALM_NAME', '$VMANGOS_REALM_IP', '$VMANGOS_REALM_PORT', '$VMANGOS_REALM_ICON', '$VMANGOS_REALM_FLAGS', '$VMANGOS_TIMEZONE', '$VMANGOS_ALLOWED_SECURITY_LEVEL', '$VMANGOS_POPULATION', '$VMANGOS_GAMEBUILD_MIN', '$VMANGOS_GAMEBUILD_MAX', '$VMANGOS_FLAG', '');"
 
 echo "[VMaNGOS]: Database creation complete!"
