bb90a24467769788ba42fa0f84164a33a4df9ced
flyingfrog23 <andrin.straessler@outlook.com>
2023-06-28 15:35:21 +0200

    Update generate-db-2.sh

--- docker/database/generate-db-2.sh
+++ docker/database/generate-db-2.sh
@@ -15,13 +15,9 @@ echo "[VMaNGOS]: Importing characters..."
 mariadb -u mangos -p$MYSQL_ROOT_PASSWORD characters < /src/github_core/sql/characters.sql
 
 echo "[VMaNGOS]: Importing database updates..."
-[ -e /opt/core/sql/migrations/world_db_updates.sql ]
   mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos < /src/github_core/sql/migrations/world_db_updates.sql
-[ -e /opt/core/sql/migrations/characters_db_updates.sql ]
   mariadb -u mangos -p$MYSQL_ROOT_PASSWORD characters < /src/github_core/sql/migrations/characters_db_updates.sql
-[ -e /opt/core/sql/migrations/logon_db_updates.sql ]
   mariadb -u mangos -p$MYSQL_ROOT_PASSWORD realmd < /src/github_core/sql/migrations/logon_db_updates.sql
-[ -e /opt/core/sql/migrations/logs_db_updates.sql ]
   mariadb -u mangos -p$MYSQL_ROOT_PASSWORD logs < /src/github_core/sql/migrations/logs_db_updates.sql
 
 echo "[VMaNGOS]: Upgrading mysql..."
