980109b8d19d9d6ad589ea2fda11964634118656
flyingfrog23 <andrin.straessler@outlook.com>
2023-07-06 14:39:08 +0200

    Update 11-world-database-import-migrations.sh

--- 11-world-database-import-migrations.sh
+++ 11-world-database-import-migrations.sh
@@ -12,26 +12,21 @@ get_script_path() {
 repository_path=$(dirname "$(get_script_path "$0")")
 cd "$repository_path"
 
-echo "[VMaNGOS]: Removing old target directory..."
+# Start
 
+echo "[VMaNGOS]: Removing old target directory..."
 rm -r ./vol/core_github
 
 echo "[VMaNGOS]: Cloning github repository..."
-
 git clone $VMANGOS_GIT_SOURCE_CORE_URL ./vol/core_github/
 
 echo "[VMaNGOS]: Cloning github repository finished."
-
 echo "[VMaNGOS]: Merging VMaNGOS core migrations..."
-
 cd ./vol/core_github/sql/migrations
 ./merge.sh
 cd "$repository_path"
 
-echo "[VMaNGOS]: VMaNGOS data prepared."
-
 echo "[VMaNGOS]: Importing world db migrations..."
-
 docker exec vmangos_database /bin/sh 'mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos < /vol/core_github/sql/migrations/world_db_updates.sql'
 
 echo "[VMaNGOS]: Import finished."
