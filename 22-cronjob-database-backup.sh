6616fd8614a8748d9973224d9e4fb0187fa5f472
flyingfrog23 <andrin.straessler@outlook.com>
2023-07-06 13:50:04 +0200

    Update 22-cronjob-database-backup.sh

--- 22-cronjob-database-backup.sh
+++ 22-cronjob-database-backup.sh
@@ -18,11 +18,8 @@ cd "$repository_path"
 
 echo "[VMaNGOS]: Backing up databases..."
 
-docker exec vmangos_database /bin/sh \
-  'mariadb-dump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD --single-transaction characters > /backup/"$date_time"_characters.sql'
-docker exec vmangos_database /bin/sh \
-  'mariadb-dump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD --single-transaction realmd > /backup/"$date_time"_realmd.sql'
-docker exec vmangos_database /bin/sh \
-  'mariadb-dump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD --single-transaction logs > /backup/"$date_time"_logs.sql'
+docker exec vmangos_database /bin/sh 'mariadb-dump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD --single-transaction characters > /backup/"$date_time"_characters.sql'
+docker exec vmangos_database /bin/sh 'mariadb-dump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD --single-transaction realmd > /backup/"$date_time"_realmd.sql'
+docker exec vmangos_database /bin/sh 'mariadb-dump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD --single-transaction logs > /backup/"$date_time"_logs.sql'
 
 echo "[VMaNGOS]: Backup complete!"
