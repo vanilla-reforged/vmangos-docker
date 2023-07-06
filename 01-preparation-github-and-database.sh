db6118fd1674cc2aecbd7dec78656a2ccb9c922b
flyingfrog23 <andrin.straessler@outlook.com>
2023-07-06 13:35:12 +0200

    Update 01-preparation-github-and-database.sh

--- 01-preparation-github-and-database.sh
+++ 01-preparation-github-and-database.sh
@@ -12,12 +12,12 @@ get_script_path() {
 repository_path=$(dirname "$(get_script_path "$0")")
 cd "$repository_path"
 
-echo "[VMaNGOS]: Removing old target directories"
+echo "[VMaNGOS]: Removing old target directories..."
 
 rm -r ./vol/core_github
 rm -r ./vol/database_github
 
-echo "[VMaNGOS]: Cloning github repositories."
+echo "[VMaNGOS]: Cloning github repositories..."
 
 git clone $VMANGOS_GIT_SOURCE_CORE_URL ./vol/core_github/
 git clone $VMANGOS_GIT_SOURCE_DATABASE_URL ./vol/database_github/
