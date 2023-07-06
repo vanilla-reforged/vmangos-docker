4e29b9335f293d2beefc56a8ec055117d4b96f01
flyingfrog23 <andrin.straessler@outlook.com>
2023-07-06 13:13:53 +0200

    Update generate-db.sh

--- docker/database/generate-db.sh
+++ docker/database/generate-db.sh
@@ -2,25 +2,25 @@
 
 echo "[VMaNGOS]: Creating databases..."
 
-CREATE DATABASE IF NOT EXISTS realmd DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
-CREATE DATABASE IF NOT EXISTS characters DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
-CREATE DATABASE IF NOT EXISTS mangos DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
-CREATE DATABASE IF NOT EXISTS logs DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+mariadb -u mangos mangos CREATE DATABASE IF NOT EXISTS realmd DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+mariadb -u mangos mangos CREATE DATABASE IF NOT EXISTS characters DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+mariadb -u mangos mangos CREATE DATABASE IF NOT EXISTS mangos DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+mariadb -u mangos mangos CREATE DATABASE IF NOT EXISTS logs DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 
 echo "[VMaNGOS]: Creating user..."
 
-CREATE USER 'mangos'@'localhost' IDENTIFIED BY 'mangos';
-SET PASSWORD FOR 'mangos'@'localhost' = PASSWORD('mangos');
+mariadb -u mangos mangos CREATE USER 'mangos'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+mariadb -u mangos mangos SET PASSWORD FOR 'mangos'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
 
 echo "[VMaNGOS]: Granting privileges for user..."
 
-GRANT ALL PRIVILEGES ON *.* TO 'mangos'@'%' IDENTIFIED BY 'mangos';
-FLUSH PRIVILEGES;
-GRANT ALL ON realmd.* TO mangos@'localhost' WITH GRANT OPTION;
-GRANT ALL ON characters.* TO mangos@'localhost' WITH GRANT OPTION;
-GRANT ALL ON mangos.* TO mangos@'localhost' WITH GRANT OPTION;
-GRANT ALL ON logs.* TO mangos@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+mariadb -u mangos mangos GRANT ALL PRIVILEGES ON *.* TO 'mangos'@'%' IDENTIFIED BY 'mangos';
+mariadb -u mangos mangos FLUSH PRIVILEGES;
+mariadb -u mangos mangos GRANT ALL ON realmd.* TO mangos@'localhost' WITH GRANT OPTION;
+mariadb -u mangos mangos GRANT ALL ON characters.* TO mangos@'localhost' WITH GRANT OPTION;
+mariadb -u mangos mangos GRANT ALL ON mangos.* TO mangos@'localhost' WITH GRANT OPTION;
+mariadb -u mangos mangos GRANT ALL ON logs.* TO mangos@'localhost' WITH GRANT OPTION;
+mariadb -u mangos mangos FLUSH PRIVILEGES;
 
 echo "[VMaNGOS]: Importing databases..."
 
