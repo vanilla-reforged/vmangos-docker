cfd9c5bb49e627669b54735a422e0ab2b3b7a2e9
flyingfrog23 <andrin.straessler@outlook.com>
2023-07-06 13:14:49 +0200

    Update backup-db-all.sh

--- docker/database/backup-db-all.sh
+++ docker/database/backup-db-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-  'mysqldump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD mangos > /backup/mangos.sql'
 docker-compose exec -T vmangos_database sh -c \
-  'mysqldump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD characters > /backup/characters.sql'
+'mysqldump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD mangos > /backup/mangos.sql'
 docker-compose exec -T vmangos_database sh -c \
-  'mysqldump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD realmd > /backup/realmd.sql'
+'mysqldump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD characters > /backup/characters.sql'
+docker-compose exec -T vmangos_database sh -c \
+'mysqldump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD realmd > /backup/realmd.sql'
