ece2201dc2a19e248a02b6a1ceb29167a49e8b60
flyingfrog23 <andrin.straessler@outlook.com>
2023-07-06 15:29:51 +0200

    Update generate-db.sh

--- docker/database/generate-db.sh
+++ docker/database/generate-db.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "[VMaNGOS]: Creating databases..."
-mariadb -u mangos mangos CREATE DATABASE IF NOT EXISTS realmd DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
-mariadb -u mangos mangos CREATE DATABASE IF NOT EXISTS characters DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
-mariadb -u mangos mangos CREATE DATABASE IF NOT EXISTS mangos DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
-mariadb -u mangos mangos CREATE DATABASE IF NOT EXISTS logs DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+mariadb -u root CREATE DATABASE IF NOT EXISTS realmd DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+mariadb -u root  CREATE DATABASE IF NOT EXISTS characters DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+mariadb -u root  CREATE DATABASE IF NOT EXISTS mangos DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+mariadb -u root  CREATE DATABASE IF NOT EXISTS logs DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 
 echo "[VMaNGOS]: Creating user..."
-mariadb -u mangos mangos CREATE USER 'mangos'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-mariadb -u mangos mangos SET PASSWORD FOR 'mangos'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
+mariadb -u root mangos CREATE USER 'mangos'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos SET PASSWORD FOR 'mangos'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
 
 echo "[VMaNGOS]: Granting privileges for user..."
 mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos GRANT ALL PRIVILEGES ON *.* TO 'mangos'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
