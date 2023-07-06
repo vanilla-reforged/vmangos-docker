bcafc1d089b9120e7ab91c85fff9f850cca16648
flyingfrog23 <andrin.straessler@outlook.com>
2023-07-06 15:32:44 +0200

    Update generate-db.sh

--- docker/yesilcms/generate-db.sh
+++ docker/yesilcms/generate-db.sh
@@ -8,13 +8,12 @@ CREATE USER '[cms_user]'@'[host]' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD_YESILCMS';
 
 echo "[YesilCMS]: Granting privileges for user..."
 
-TODO
-mariadb -u cms_user mangos GRANT ALL PRIVILEGES ON *.* TO 'mangos'@'%' IDENTIFIED BY 'mangos';
-mariadb -u mangos mangos FLUSH PRIVILEGES;
-mariadb -u mangos mangos GRANT ALL ON realmd.* TO mangos@'localhost' WITH GRANT OPTION;
-mariadb -u mangos mangos GRANT ALL ON characters.* TO mangos@'localhost' WITH GRANT OPTION;
-mariadb -u mangos mangos GRANT ALL ON mangos.* TO mangos@'localhost' WITH GRANT OPTION;
-mariadb -u mangos mangos GRANT ALL ON logs.* TO mangos@'localhost' WITH GRANT OPTION;
-mariadb -u mangos mangos FLUSH PRIVILEGES;
+mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT ALL PRIVILEGES ON *.* TO 'mangos'@'%' IDENTIFIED BY 'mangos';
+mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db FLUSH PRIVILEGES;
+mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT ALL ON realmd.* TO mangos@'localhost' WITH GRANT OPTION;
+mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT ALL ON characters.* TO mangos@'localhost' WITH GRANT OPTION;
+mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT ALL ON mangos.* TO mangos@'localhost' WITH GRANT OPTION;
+mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT ALL ON logs.* TO mangos@'localhost' WITH GRANT OPTION;
+mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db FLUSH PRIVILEGES;
 
 echo "[YesilCMS]: Database creation complete!"
