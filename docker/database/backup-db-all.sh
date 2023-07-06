bbecd40c8608a8c719956d64e64cd1301583eedf
flyingfrog23 <andrin.straessler@outlook.com>
2023-07-06 13:15:41 +0200

    Delete backup-db-all.sh

--- docker/database/backup-db-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker-compose exec -T vmangos_database sh -c \
-'mysqldump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD mangos > /backup/mangos.sql'
-docker-compose exec -T vmangos_database sh -c \
-'mysqldump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD characters > /backup/characters.sql'
-docker-compose exec -T vmangos_database sh -c \
-'mysqldump -h 127.0.0.1 -u root -p$MYSQL_ROOT_PASSWORD realmd > /backup/realmd.sql'
