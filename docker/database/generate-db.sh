6896d19519b1362b62fd91473505782d73d90381
flyingfrog23 <andrin.straessler@outlook.com>
2023-07-07 15:25:12 +0200

    Update generate-db.sh

--- docker/database/generate-db.sh
+++ docker/database/generate-db.sh
@@ -21,21 +21,23 @@ sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 
 echo "[VMaNGOS]: Importing databases…"
 echo "[VMaNGOS]: Importing world…"
-sudo mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos < /vol/database_github/$VMANGOS_WORLD_DATABASE.sql
+sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos < /vol/database_github/$VMANGOS_WORLD_DATABASE.sql
 echo "[VMaNGOS]: Importing logon…"
-sudo mariadb -u mangos -p$MYSQL_ROOT_PASSWORD realmd < /vol/core_github/sql/logon.sql
+sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD realmd < /vol/core_github/sql/logon.sql
 echo "[VMaNGOS]: Importing logs…"
-sudo mariadb -u mangos -p$MYSQL_ROOT_PASSWORD logs < /vol/core_github/sql/logs.sql
+sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD logs < /vol/core_github/sql/logs.sql
 echo "[VMaNGOS]: Importing characters…"
-sudo mariadb -u mangos -p$MYSQL_ROOT_PASSWORD characters < /vol/core_github/sql/characters.sql
+sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD characters < /vol/core_github/sql/characters.sql
 echo "[VMaNGOS]: Importing migrations…"
-sudo mariadb -u mangos -p$MYSQL_ROOT_PASSWORD mangos < /vol/core_github/sql/migrations/world_db_updates.sql
-sudo mariadb -u mangos -p$MYSQL_ROOT_PASSWORD characters < /vol/core_github/sql/migrations/characters_db_updates.sql
-sudo mariadb -u mangos -p$MYSQL_ROOT_PASSWORD realmd < /vol/core_github/sql/migrations/logon_db_updates.sql
-sudo mariadb -u mangos -p$MYSQL_ROOT_PASSWORD logs < /vol/core_github/sql/migrations/logs_db_updates.sql
+sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos < /vol/core_github/sql/migrations/world_db_updates.sql
+sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD characters < /vol/core_github/sql/migrations/characters_db_updates.sql
+sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD realmd < /vol/core_github/sql/migrations/logon_db_updates.sql
+sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD logs < /vol/core_github/sql/migrations/logs_db_updates.sql
 
 echo "[VMaNGOS]: Upgrading mysql…"
-sudo mariadb_upgrade -u mangos -p$MYSQL_ROOT_PASSWORD
+sudo mariadb-upgrade -u root -p$MYSQL_ROOT_PASSWORD
 
 echo "[VMaNGOS]: Configuring default realm…"
-sudo mariadb -u mangos -p$MYSQL_ROOT_PASSWORD -e "INSERT INTO realmd.realmlist (name, address, port, icon, realmflags, timezone, allowedSecurityLevel, population, gamebuild_min, gamebuild_max, flag, realmbuilds) VALUES ('$VMANGOS_REALM_NAME', '$VMANGOS_REALM_IP', '$VMANGOS_REALM_PORT', '$VMANGOS_REALM_ICON', '$VMANGOS_REALM_FLAGS', '$VMANGOS_TIMEZONE', '$VMANGOS_ALLOWED_SECURITY_LEVEL', '$VMANGOS_POP
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "INSERT INTO realmd.realmlist (name, address, port, icon, realmflags, timezone, allowedSecurityLevel, population, gamebuild_min, gamebuild_max, flag, realmbuilds) VALUES ('$VMANGOS_REALM_NAME', '$VMANGOS_REALM_IP', '$VMANGOS_REALM_PORT', '$VMANGOS_REALM_ICON', '$VMANGOS_REALM_FLAGS', '$VMANGOS_TIMEZONE', '$VMANGOS_ALLOWED_SECURITY_LEVEL', '$VMANGOS_POPULATION', '$VMANGOS_GAMEBUILD_MIN', '$VMANGOS_GAMEBUILD_MAX', '$VMANGOS_FLAG', '');"
+
+echo "[VMaNGOS]: Database creation complete!"
