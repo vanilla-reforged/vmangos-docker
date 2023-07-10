f34fa80de7bf9c43b4f59627520fdc67c4beddbe
vanilla-reforged <vanilla.reforged@outlook.com>
2023-07-10 17:09:48 +0200

    Update generate-db.sh

--- docker/yesilcms/generate-db.sh
+++ docker/yesilcms/generate-db.sh
@@ -4,16 +4,12 @@ echo "[YesilCMS]: Creating database..."
 mariadb -u root CREATE DATABASE [cms_db];;
 
 echo "[YesilCMS]: Creating user..."
-CREATE USER '[cms_user]'@'[host]' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD_YESILCMS';
+mariadb -u root -p$MYSQL_ROOT_PASSWORD_YESILCMS USER '[cms_user]'@'[host]' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD_YESILCMS';
 
 echo "[YesilCMS]: Granting privileges for user..."
 
-mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT ALL PRIVILEGES ON *.* TO 'mangos'@'%' IDENTIFIED BY 'mangos';
-mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db FLUSH PRIVILEGES;
-mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT ALL ON realmd.* TO mangos@'localhost' WITH GRANT OPTION;
-mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT ALL ON characters.* TO mangos@'localhost' WITH GRANT OPTION;
-mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT ALL ON mangos.* TO mangos@'localhost' WITH GRANT OPTION;
-mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT ALL ON logs.* TO mangos@'localhost' WITH GRANT OPTION;
+mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT USAGE ON *.* TO '[cms_user]'@'[localhost]';
+mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db GRANT SELECT, EXECUTE, SHOW VIEW, ALTER, ALTER ROUTINE, CREATE, CREATE ROUTINE, CREATE TEMPORARY TABLES, CREATE VIEW, DELETE, DROP, EVENT, INDEX, INSERT, REFERENCES, TRIGGER, UPDATE, LOCK TABLES  ON [cms_db].* TO '[cms_user]'@'[host]';
 mariadb -u cms_user -p$MYSQL_ROOT_PASSWORD_YESILCMS cms_db FLUSH PRIVILEGES;
 
 echo "[YesilCMS]: Database creation complete!"
