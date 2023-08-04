54c94f4d0cf50aea615c5a69b49d52720fc3e163
vanilla-reforged <vanilla.reforged@outlook.com>
2023-08-04 14:34:14 +0200

    Update 13-recompile-core.sh

--- 13-recompile-core.sh
+++ 13-recompile-core.sh
@@ -17,9 +17,6 @@ cd "$repository_path"
 echo "[VMaNGOS]: Removing old target directory..."
 rm -r ./vol/core_github
 
-echo "[VMaNGOS]: Removing contents of ccache directory..."
-rm -r ./vol/ccache/*
-
 echo "[VMaNGOS]: Cloning github repository..."
 git clone $VMANGOS_GIT_SOURCE_CORE_URL ./vol/core_github/
 
