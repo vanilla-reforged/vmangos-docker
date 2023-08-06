809b259c9f40265f01c35f2d27e5a429e7a0b34d
vanilla-reforged <vanilla.reforged@outlook.com>
2023-08-06 15:06:23 +0200

    Update 21-database-backup.sh

--- 21-database-backup.sh
+++ 21-database-backup.sh
@@ -9,8 +9,8 @@ timestamp=$(date +%Y%m%d_%H%M%S)
 # Start
 
 echo "[VMaNGOS]: Backing up databases..."
-docker exec vmangos_database mariadb-dump -h 127.0.0.1 -u mangos -p$MYSQL_ROOT_PASSWORD --single-transaction characters > ./vol/backup/"$timestamp"_characters.sql
-docker exec vmangos_database mariadb-dump -h 127.0.0.1 -u mangos -p$MYSQL_ROOT_PASSWORD --single-transaction realmd > ./vol/backup/"$timestamp"_realmd.sql
-docker exec vmangos_database mariadb-dump -h 127.0.0.1 -u mangos -p$MYSQL_ROOT_PASSWORD --single-transaction logs > ./vol/backup/"$timestamp"_logs.sql
+docker exec vmangos_database mariadb-dump -h 127.0.0.1 -u mangos -p$MYSQL_ROOT_PASSWORD --single-transaction characters > ./vol/backup/characters_"$timestamp".sql
+docker exec vmangos_database mariadb-dump -h 127.0.0.1 -u mangos -p$MYSQL_ROOT_PASSWORD --single-transaction realmd > ./vol/backup/realmd_"$timestamp".sql
+docker exec vmangos_database mariadb-dump -h 127.0.0.1 -u mangos -p$MYSQL_ROOT_PASSWORD --single-transaction logs > ./vol/backup/logs_"$timestamp".sql
 
 echo "[VMaNGOS]: Backup complete!"
