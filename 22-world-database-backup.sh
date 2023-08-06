79270cdb6f7a059351f41fa6b1cdd4ff2fb52e19
vanilla-reforged <vanilla.reforged@outlook.com>
2023-08-06 15:06:43 +0200

    Update 22-world-database-backup.sh

--- 22-world-database-backup.sh
+++ 22-world-database-backup.sh
@@ -9,6 +9,6 @@ timestamp=$(date +%Y%m%d_%H%M%S)
 # Start
 
 echo "[VMaNGOS]: Backing up world database..."
-docker exec vmangos_database mariadb-dump -h 127.0.0.1 -u mangos -p$MYSQL_ROOT_PASSWORD --single-transaction mangos > ./vol/backup/"$timestamp"_mangos.sql
+docker exec vmangos_database mariadb-dump -h 127.0.0.1 -u mangos -p$MYSQL_ROOT_PASSWORD --single-transaction mangos > ./vol/backup/mangos_"$timestamp".sql
 
 echo "[VMaNGOS]: Backup complete!"
