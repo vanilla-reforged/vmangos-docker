823072ab4b90d1ece18bb8ecc2018ac5638b4f82
vanilla-reforged <vanilla.reforged@outlook.com>
2023-08-10 13:36:06 +0200

    Update recreate-world-db.sh

--- docker/database/recreate-world-db.sh
+++ docker/database/recreate-world-db.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo "[VMaNGOS]: Recreating world database..."
-sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos DROP DATABASE mangos;
-sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos CREATE DATABASE mangos DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
-sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos GRANT ALL PRIVILEGES ON *.* TO 'mangos'@'%' IDENTIFIED BY 'mangos';
-sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos FLUSH PRIVILEGES;
-sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos GRANT ALL ON mangos.* TO mangos@'localhost' WITH GRANT OPTION;
-sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos FLUSH PRIVILEGES;
+mariadb -u root -p$MYSQL_ROOT_PASSWORD DROP DATABASE mangos;
+mariadb -u root -p$MYSQL_ROOT_PASSWORD CREATE DATABASE mangos DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+mariadb -u root -p$MYSQL_ROOT_PASSWORD GRANT ALL PRIVILEGES ON *.* TO 'mangos'@'%' IDENTIFIED BY 'mangos';
+mariadb -u root -p$MYSQL_ROOT_PASSWORD FLUSH PRIVILEGES;
+mariadb -u root -p$MYSQL_ROOT_PASSWORD GRANT ALL ON mangos.* TO mangos@'localhost' WITH GRANT OPTION;
+mariadb -u root -p$MYSQL_ROOT_PASSWORD FLUSH PRIVILEGES;
 
 echo "[VMaNGOS]: Importing databases..."
 echo "[VMaNGOS]: Importing world..."
-sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos < /vol/database_github/$VMANGOS_WORLD_DATABASE.sql
+mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos < /vol/database_github/$VMANGOS_WORLD_DATABASE.sql
 
 echo "[VMaNGOS]: Importing world db migrations..."
-sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos < /vol/core_github/sql/migrations/world_db_updates.sql
+mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos < /vol/core_github/sql/migrations/world_db_updates.sql
 
 echo "[VMaNGOS]: Import finished."
