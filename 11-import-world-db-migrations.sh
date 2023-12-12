c47fcfd5885dc910a7a38eec1b93c917c62aa08b
vanilla-reforged <vanilla.reforged@outlook.com>
2023-12-12 16:54:22 +0100

    Update 11-import-world-db-migrations.sh

--- 11-import-world-db-migrations.sh
+++ 11-import-world-db-migrations.sh
@@ -13,13 +13,10 @@ repository_path=$(dirname "$(get_script_path "$0")")
 cd "$repository_path"
 
 # Start
-
+# Restart environment to make new files visible inside container volume
 docker compose down
-
 sleep 30s
-
 docker compose up -d
-
 sleep 30s
-
+# Execute sh script inside container
 docker exec vmangos_database /import-world-db-migrations.sh
