9f245b47934cdcc17e1b5928125c77f29ec53eeb
vanilla-reforged <vanilla.reforged@outlook.com>
2024-05-28 15:00:35 +0200

    Update import-world-db-migrations.sh

--- docker/database/import-world-db-migrations.sh
+++ docker/database/import-world-db-migrations.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "[VMaNGOS]: Importing migrations…"
-sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos < /vol/core_github/sql/migrations/world_db_updates.sql
+sudo mariadb -u root -p$MYSQL_ROOT_PASSWORD -f mangos < /vol/core_github/sql/migrations/world_db_updates.sql
