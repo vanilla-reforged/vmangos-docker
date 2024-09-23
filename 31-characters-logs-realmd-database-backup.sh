d520b0e7e7bdb773549fb9ea27a7ad4649ddd240
vanilla-reforged <vanilla.reforged@outlook.com>
2024-09-23 08:59:08 +0000

    update

--- 31-characters-logs-realmd-database-backup.sh
+++ 31-characters-logs-realmd-database-backup.sh
@@ -5,8 +5,6 @@ source "$(dirname "$0")/.env-script"
 
 # Configuration
 CONTAINER_BACKUP_DIR="/vol/backup"  # Backup directory inside the Docker container
-DB_USER="mangos"  # Database username
-DB_PASS="$MYSQL_ROOT_PASSWORD"  # Database password sourced from .env-script
 CONTAINER_NAME="vmangos-database"  # Docker container name
 
 # Function to send a message to Discord
@@ -21,9 +19,9 @@ send_discord_message() {
 # Function to create a full SQL dump (daily)
 create_full_backup() {
     echo "Creating full SQL dump inside the container..."
-    
-    # Dump specific databases: characters, logs, realmd
-    docker exec $CONTAINER_NAME bash -c "mysqldump --user=$DB_USER --password=$DB_PASS --databases characters logs realmd > $CONTAINER_BACKUP_DIR/full_backup.sql"
+
+    # Use mariadb-dump to dump specific databases: characters, logs, realmd
+    docker exec $CONTAINER_NAME bash -c "mariadb-dump --user=$DB_USER --password=$DB_PASS --databases characters logs realmd > $CONTAINER_BACKUP_DIR/full_backup.sql"
     
     if [[ $? -eq 0 ]]; then
         echo "Full SQL dump created successfully inside the container."
