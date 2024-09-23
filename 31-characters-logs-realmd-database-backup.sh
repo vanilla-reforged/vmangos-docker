7be18c83b4bf5707158efc23744220a315524602
vanilla-reforged <vanilla.reforged@outlook.com>
2024-09-23 09:00:51 +0000

    update

--- 31-characters-logs-realmd-database-backup.sh
+++ 31-characters-logs-realmd-database-backup.sh
@@ -4,7 +4,10 @@
 source "$(dirname "$0")/.env-script"
 
 # Configuration
+HOST_BACKUP_DIR="./vol/backup"  # Local backup directory on the host
 CONTAINER_BACKUP_DIR="/vol/backup"  # Backup directory inside the Docker container
+DB_USER="mangos"  # Database username from .env-script
+DB_PASS="$MYSQL_ROOT_PASSWORD"  # Database password from .env-script
 CONTAINER_NAME="vmangos-database"  # Docker container name
 
 # Function to send a message to Discord
@@ -20,16 +23,16 @@ send_discord_message() {
 create_full_backup() {
     echo "Creating full SQL dump inside the container..."
 
-    # Use mariadb-dump to dump specific databases: characters, logs, realmd
+    # Dump specific databases: characters, logs, realmd
     docker exec $CONTAINER_NAME bash -c "mariadb-dump --user=$DB_USER --password=$DB_PASS --databases characters logs realmd > $CONTAINER_BACKUP_DIR/full_backup.sql"
-    
+
     if [[ $? -eq 0 ]]; then
         echo "Full SQL dump created successfully inside the container."
-        
+
         # Compress the SQL dump
         echo "Compressing full SQL dump..."
         docker exec $CONTAINER_NAME bash -c "7z a $CONTAINER_BACKUP_DIR/full_backup.7z $CONTAINER_BACKUP_DIR/full_backup.sql"
-        
+
         if [[ $? -eq 0 ]]; then
             echo "Full SQL dump compressed successfully."
             send_discord_message "Daily SQL dump backup completed successfully."
@@ -45,7 +48,5 @@ create_full_backup() {
     fi
 }
 
-# Execute full backup
+# Main function to start the backup
 create_full_backup
-
-exit 0
