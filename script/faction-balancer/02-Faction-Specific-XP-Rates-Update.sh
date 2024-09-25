221a8624ffbf61c21db2e8ed75930aa885644542
vanilla-reforged <vanilla.reforged@outlook.com>
2024-09-25 13:57:19 +0000

    update

--- script/faction-balancer/02-Faction-Specific-XP-Rates-Update.sh
+++ script/faction-balancer/02-Faction-Specific-XP-Rates-Update.sh
@@ -77,14 +77,14 @@ restart_server() {
     echo "Restarting VMangos server..."
 
     # Check if the VMangos Docker container is running
-    if ! docker ps --format "{{.Names}}" | grep -q "^vmangos-mangos$"; then
+    if ! sudo docker ps --format "{{.Names}}" | grep -q "^vmangos-mangos$"; then
         echo "Error: VMangos container 'vmangos-mangos' is not running."
         exit 1
     fi
 
     # Use expect to attach to the container, send the restart command, and detach
     expect << EOF
-    spawn docker attach vmangos-mangos
+    spawn sudo docker attach vmangos-mangos
     expect "#"
     send "server restart 900\r"
     expect "#"
