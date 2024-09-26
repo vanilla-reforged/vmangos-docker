6745e17aa4d8b8b30863b34dccabe6b7aab4d0d0
vanilla-reforged <vanilla.reforged@outlook.com>
2024-09-26 12:41:45 +0200

    Update 01-mangos-database-backup.sh

--- script/backup/01-mangos-database-backup.sh
+++ script/backup/01-mangos-database-backup.sh
@@ -4,7 +4,7 @@
 cd "$(dirname "$0")"
 
 # Run the backup script inside the container
-docker exec vmangos-database /01-mangos-database-backup.sh \
+sudo docker exec vmangos-database /home/default/scripts/01-mangos-database-backup.sh \
   || { echo "Failed to execute backup inside container"; exit 1; }
 
 echo "[VMaNGOS]: Backup script executed inside container!"
