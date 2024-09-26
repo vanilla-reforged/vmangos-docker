02424e289002064cd5c7f3f671a14f457925b58c
vanilla-reforged <vanilla.reforged@outlook.com>
2024-09-26 12:39:52 +0200

    Update 01-docker-7zip-ufw-jq-install.sh

--- script/setup/01-docker-7zip-ufw-jq-install.sh
+++ script/setup/01-docker-7zip-ufw-jq-install.sh
@@ -139,10 +139,10 @@ echo "Configuring sudoers for Docker commands for user '$LOCAL_USER'..."
 
 # Ensure sudoers file is updated to allow passwordless sudo for specific Docker commands
 echo "$LOCAL_USER ALL=(ALL) NOPASSWD: \
-    /usr/bin/docker attach vmangos-mangos*, \
+    /usr/bin/docker attach vmangos-mangos, \
     /usr/bin/docker ps, \
     /usr/bin/docker stats, \
-    /usr/bin/docker compose, \
+    /usr/bin/docker compose *, \
     /usr/bin/docker exec vmangos-database /home/default/scripts/01-mangos-database-backup.sh, \
     /usr/bin/docker exec vmangos-database /home/default/scripts/01-population-balance-collect.sh, \
     /usr/bin/docker exec vmangos-database /home/default/scripts/02-characters-logs-realmd-databases-backup.sh, \
