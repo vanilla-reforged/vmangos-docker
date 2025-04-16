a0d14e47c7aa5f4e4b7e5792166ccd508af3ba9e
vanilla-reforged <vanilla.reforged@outlook.com>
2025-04-16 17:07:48 +0200

    Update 01-mangos-database-backup.sh

--- docker/database/01-mangos-database-backup.sh
+++ docker/database/01-mangos-database-backup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+# Run with nice (CPU priority 19) and ionice (I/O priority class 2, level 7)
+# This is a heavy backup process, so use maximum niceness
+nice -n 19 ionice -c 2 -n 7 bash -c '
 # Define variables
 BACKUP_DIR="/vol/backup"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 DATABASE="mangos"
-
 # Function to back up a database
 backup_database() {
   local db_name=$1
@@ -12,8 +13,7 @@ backup_database() {
   mariadb-dump -h 127.0.0.1 -u mangos -p"$MYSQL_ROOT_PASSWORD" --single-transaction "$db_name" > "$BACKUP_DIR/${db_name}_${TIMESTAMP}.sql" \
     || { echo "Failed to back up $db_name"; exit 1; }
 }
-
 # Backup the database
 backup_database "$DATABASE"
-
 echo "[VMaNGOS]: Backup complete!"
+'
