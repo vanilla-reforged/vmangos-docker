e27ff8757112905f3267df7df0a4233100c50eb9
vanilla-reforged <vanilla.reforged@outlook.com>
2025-12-11 17:45:10 +0100

    Update 01-vmangos-database-migrations-import.sh

--- script/management/01-vmangos-database-migrations-import.sh
+++ script/management/01-vmangos-database-migrations-import.sh
@@ -9,9 +9,9 @@ source ./../../.env-script  # Adjusted to correctly load .env-script from the pr
 # Start
 echo "[VMaNGOS]: Importing migrations…"
 docker exec -i vmangos-database mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos < "$DOCKER_DIRECTORY/vol/core-github/sql/migrations/world_db_updates.sql"
-docker exec -i vmangos-database mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos < "$DOCKER_DIRECTORY/vol/core-github/sql/migrations/characters_db_updates.sql"
-docker exec -i vmangos-database mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos < "$DOCKER_DIRECTORY/vol/core-github/sql/migrations/logon_db_updates.sql"
-docker exec -i vmangos-database mariadb -u root -p$MYSQL_ROOT_PASSWORD mangos < "$DOCKER_DIRECTORY/vol/core-github/sql/migrations/logs_db_updates.sql"
+docker exec -i vmangos-database mariadb -u root -p$MYSQL_ROOT_PASSWORD characters < "$DOCKER_DIRECTORY/vol/core-github/sql/migrations/characters_db_updates.sql"
+docker exec -i vmangos-database mariadb -u root -p$MYSQL_ROOT_PASSWORD realmd < "$DOCKER_DIRECTORY/vol/core-github/sql/migrations/logon_db_updates.sql"
+docker exec -i vmangos-database mariadb -u root -p$MYSQL_ROOT_PASSWORD logs < "$DOCKER_DIRECTORY/vol/core-github/sql/migrations/logs_db_updates.sql"
 
 
 echo "[VMaNGOS]: Restarting environment..."
